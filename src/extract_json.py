338ffc03b7ebd112c9337c1efc1a587099d1da2c
Henning Fischer <kontakt@henning-fischer.net>
2019-01-14 16:57:11 +0100

    json extractor ready

--- src/extract_json.py
+++ src/extract_json.py
@@ -2,35 +2,49 @@ import json
 
 class Extract_json:
 
+    # first convert json_data into dict
+    # second take every key from search_list and split them with "/" to get a list of key_elements(path_element_list)
+    # give json_data and path_element_list to compare method and get a solution dict
+    # give solution_dict to convert method and return the solution_json
     def get_json_from_path(self, json_string, search_key_list):
 
         solution_dict = {}
 
-        json_data = json.loads(json_string)
+        dict_data = self._convert_json_to_dict(json_string)
 
         for key in search_key_list:
 
             path_element_list = key.split("/")
 
-            solution_dict[key] = self._compare_search_key_with_json(json_data, path_element_list)
+            solution_dict[key] = self._compare_search_key_with_json(dict_data, path_element_list)
 
-        json_string_solution = json.dumps(solution_dict)
+        json_string_solution = self._convert_dict_to_json(solution_dict)
 
         return json_string_solution
 
-    def _compare_search_key_with_json(self, json_data, path_element_list):
+    # take every path_element and check if it is a key in json_data
+    # if not it returns the value None
+    # if yes goes into value of dict_data[key]
+    def _compare_search_key_with_json(self, dict_data, path_element_list):
 
         for path_element_list_element in path_element_list:
 
-            if path_element_list_element not in json_data:
+            if path_element_list_element not in dict_data:
 
                 return None
 
             else:
 
-                json_data = json_data[path_element_list_element]
+                dict_data = dict_data[path_element_list_element]
 
-        return json_data
+        return dict_data
 
+    def _convert_json_to_dict(self, json_string):
+
+        return json.loads(json_string)
+
+    def _convert_dict_to_json(self, solution_dict):
+
+        return json.dumps(solution_dict)
 
 
--- test/test_extract_json.py
+++ test/test_extract_json.py
@@ -9,10 +9,10 @@ class Extract_json_UnitTest(unittest.TestCase):
 
         self.extract_json = Extract_json()
         self.string_dict = {'a': 'A', 'b': 'B', 'c': 'C', 'd': {'d1': 'D1', 'Foo': 'Bar'}, 'e': 'F'}
+        self.json_data = json.dumps(self.string_dict)
+        self.dict_data = json.loads(self.json_data)
 
     def test_extract_json(self):
-
-        json_string = json.dumps(self.string_dict)
 
         search_key_list = ['b', 'd/Foo', 'e', 'H']
 
@@ -20,7 +20,7 @@ class Extract_json_UnitTest(unittest.TestCase):
 
         json_expected = json.dumps(expected_json)
 
-        solution_json = self.extract_json.get_json_from_path(json_string, search_key_list)
+        solution_json = self.extract_json.get_json_from_path(self.json_data, search_key_list)
 
         assert solution_json == json_expected
 
@@ -35,3 +35,11 @@ class Extract_json_UnitTest(unittest.TestCase):
         assert self.extract_json._compare_search_key_with_json(self.string_dict, search_key_list_element) == 'Bar'
         assert self.extract_json._compare_search_key_with_json(self.string_dict, search_key_list_element1) is None
         assert self.extract_json._compare_search_key_with_json(self.string_dict, search_key_list_element2) == 'B'
+
+    def _convert_json_to_dict(self):
+
+        assert isinstance(self.extract_json._convert_json_to_dict(self.json_data), object)
+    
+    def _convert_dict_to_json(self):
+
+        assert isinstance(self.extract_json._convert_dict_to_json(self.dict_data), dict)
